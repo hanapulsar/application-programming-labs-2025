f7055ec78987343011ea78cb0971c258f3792d4e
hanapulsar <217953127+hanapulsar@users.noreply.github.com>
2025-12-15 22:14:06 +0400

    Add barebone main_window.py, iterator, example of annotation file

---
+++ laba-5/main_window.py
@@ -0,0 +1,20 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Просмотрщик изображений")
+        self.resize(800, 600)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec_())
