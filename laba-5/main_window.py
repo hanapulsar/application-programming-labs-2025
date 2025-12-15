590de28c86efa820638ab3526e55d0cda2710fc1
hanapulsar <217953127+hanapulsar@users.noreply.github.com>
2025-12-15 22:20:03 +0400

    Add simple layout

--- laba-5/main_window.py
+++ laba-5/main_window.py
@@ -1,6 +1,8 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton, QLabel
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 
 
 class MainWindow(QMainWindow):
@@ -10,11 +12,25 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Просмотрщик изображений")
         self.resize(800, 600)
 
+        self.image_label = QLabel("Выберите файл аннотации, чтобы начать", self)
+        self.image_label.setAlignment(Qt.AlignCenter)
+
+        self.open_button = QPushButton("Выбрать файл аннотации (.csv)", self)
+
+        self.next_button = QPushButton("Следующее изображение", self)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.image_label)
+        layout.addWidget(self.open_button)
+        layout.addWidget(self.next_button)
+
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
     window = MainWindow()
     window.show()
-
     sys.exit(app.exec_())
